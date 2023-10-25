8f1c0922dacc0d0d050a62fb7127cc32913b5cf4
clovermac <chloelizabeth.cm@gmail.com>
2023-10-25 21:45:55 +0800

    the begining

---
+++ main.py
@@ -0,0 +1,48 @@
+#FEATURE 1- HOMEPAGE
+
+# DISPLAY COMPANY NAME
+print ("Welcome to OLD McDONALD'S FARM")
+# display a PRESS ENTER TO CONTINUE FEATURE 
+# user presses enter
+# what is your name?
+name = input("Enter your name    ")
+# user input
+print (f'Hi {name} , check out what Baskets we have on offer today.')
+# Hi [insert name], Welcome to Old Mcdonald's Farm. Let's get started!
+# display a PRESS ENTER TO CONTINUE FEATURE or at anytime press ! to go back
+
+
+# FEATURE 2- USER INPUT
+
+# display- How many people? 2, 4, 6
+# the user must input one of the above numbers.
+# display- Thank you [insert name]! Please choose a Basket. Vegtable, Fruit, Mixed or Suprise Me?
+# input answer
+
+
+#FEATURE 3- BUNDLES
+
+# display 2 versions of the chosen bundle.(except for suprise me) 
+#High priced bundle
+#Budget bundle
+# If suprise me is chosen then a description of what they have selected is given. 
+#input selection
+
+#FEATURE 4- ADD ON'S
+
+# with the bundle in cart a list of items are shown that can be added on with the individual prices. 
+# the user inputs the name and quantity they wish. 
+# If they wish to undo they can remove by - items and quantity.
+# continue
+
+#FEATURE 5- PURCHASE
+
+# display bundle plus add ons and total price.
+# display a PRESS ENTER TO CONTINUE FEATURE
+# [insert name] we are almost there! Please enter your full name.
+# input name 
+# And lastly we just need your phone number in case we need to contact you about your order. 
+# Input phone number 
+# CONGRATULATIONS!! Your order will be ready for pick up [enter following date]. Here is your order number [insert number over 100]
+# We recommend you take a picture of your order number or screenshot. 
+
